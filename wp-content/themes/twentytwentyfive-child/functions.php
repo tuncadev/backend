b9a58d33d1b9d999b96d4419921fff25b8bc9855
Ozgur Murat Tunca <tunca.development@gmail.com>
2025-11-27 14:44:19 +0200

    rm restrict

--- wp-content/themes/twentytwentyfive-child/functions.php
+++ wp-content/themes/twentytwentyfive-child/functions.php
@@ -11,39 +11,6 @@ add_action('template_redirect', function () {
     }
 });
 
-/* restric access */
-
-add_action('init', function () {
-    $uri = $_SERVER['REQUEST_URI'] ?? '';
-    $is_login = str_contains($uri, '/wp-login.php');
-    $is_admin = str_contains($uri, '/wp-admin');
-
-    $is_logged_in = is_user_logged_in();
-    $has_secret = isset($_GET['secret']) && $_GET['secret'] === SECRET_LOGIN_SLUG;
-
-    // Allow direct login via secret slug (with/without slash)
-    if (trim($uri, '/') === SECRET_LOGIN_SLUG) {
-        require_once ABSPATH . 'wp-login.php';
-        exit;
-    }
-
-    // 🚑 Allow redirect_to after login even if WP cookies not yet processed
-    if (isset($_GET['redirect_to'])) {
-        return;
-    }
-
-    // Let logged-in users go anywhere
-    if ($is_logged_in) return;
-
-    // Block direct access to login or admin pages
-    if (($is_login || $is_admin) && !$has_secret) {
-        status_header(404);
-        include get_404_template();
-        exit;
-    }
-});
-
-
 
 // Allow WooCommerce REST API access
 add_filter('woocommerce_rest_check_permissions', '__return_true', 10, 2);
